2a6dc1265bd03007af0d02d3add88f631731a330
Mohsen Hasani <m.hasani@alibaba.ir>
2022-03-25 15:26:07 +0430

    add and get functions in user struct

--- models/user.go
+++ models/user.go
@@ -5,3 +5,19 @@ type User struct {
 	FirstName string
 	LastName  string
 }
+
+var (
+	users  []*User
+	nextId = 1
+)
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(user User) (User, error) {
+	user.Id = nextId
+	nextId++
+	users = append(users, &user)
+	return user, nil
+}
