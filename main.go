b3f8177afcec9b5aff9f0dcd092b023ea8cd9fd6
Mohsen Hasani <m.hasani@alibaba.ir>
2022-03-25 16:53:16 +0430

    recover panic (catch exception :D)

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
+func A() {
+	defer fmt.Println("A")
+	/* defer => Defer is a language mechanism that puts your function call into a stack.
+	The deferred functions will be executed in reverse order
+	when the host function finishes regardless of whether a panic is called or not */
+
+	defer func() {
+		if r := recover(); r != nil { // recover => catch the panic
+			fmt.Printf("Panic: %+v\n", r)
+		}
+	}()
+	B()
+}
+func B() {
+	defer fmt.Println("B")
+	C()
+}
+func C() {
+	defer fmt.Println("C")
+	Break()
+}
+func Break() {
+	defer fmt.Println("D")
+	panic(errors.New("the show must go on")) // panic => exception
+}
 func main() {
-	fmt.Println("Starting web server ...")
-
-	panic("Something bad just happened ! :(")
-
-	fmt.Println("Web server started")
+	A()
 }
